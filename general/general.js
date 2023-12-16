70d12b33c6a9efda70f045f772087d37347c105b
Astenolite <vladcindea06@gmail.com>
2023-12-16 12:13:39 +0200

    test7

--- general/general.js
+++ general/general.js
@@ -4,25 +4,39 @@ document.addEventListener("DOMContentLoaded", function() {
   const absoluteURL = window.location.pathname;
   const pathArray = absoluteURL.split('/');
   const targetIndex = pathArray.indexOf("ONG");
+  const pathToFolder = "general/master-header/";
+  let newPath = "";
   console.log(pathArray);
   console.log(targetIndex);
-
+  if(targetIndex !== -1){
+    newPath = pathArray.slice(0, targetIndex + 1).join('/');
+    console.log(newPath);
+  }else{
+    const nrOfClimbs = pathArray.length - 2;
+    for(let i =1; i<=nrOfClimbs; i++){
+      newPath += "../";
+    }
+    
+  }
+  newPath += pathToFolder;
+  console.log(newPath);
 
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("masterHeader").innerHTML = xhttp.responseText;
 
       const script = document.createElement("script");
-      script.src = "/general/master-header/master-header.js";
+      script.src = newPath + "master-header.js";
       document.body.appendChild(script);
 
       const link = document.createElement("link");
       link.rel = "stylesheet";
-      link.href = "/general/master-header/header.css";
+      link.href = newPath + "header.css";
       document.body.appendChild(link);
     }
   };
 
-  xhttp.open("GET", "general/master-header/master-header.html", true);
+
+  xhttp.open("GET", newPath + "master-header.html", true);
   xhttp.send();
 });
