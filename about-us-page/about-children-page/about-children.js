138941750861d4a872effdbc01275f02273001d3
Astenolite <vladcindea06@gmail.com>
2023-12-17 16:49:19 +0200

    animatedChart

--- about-us-page/about-children-page/about-children.js
+++ about-us-page/about-children-page/about-children.js
@@ -1,3 +1,14 @@
+//let bars;
+
+const observer = new IntersectionObserver(entries =>{
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add("bar-animation");
+      entry.target.style.height = getComputedStyle(entry.target).getPropertyValue("--bar-height");
+    }
+  });
+}, {threshold: 1} );
+
 document.addEventListener("DOMContentLoaded", function() {
   const data = [204, 168, 204, 243]; // Example data for the bars
   const container = document.getElementById("chart-container");
@@ -8,7 +19,9 @@ document.addEventListener("DOMContentLoaded", function() {
   data.forEach((value, index) => {
     const bar = document.createElement("div");
     bar.className = "bar";
-    bar.style.height = value + "px";
+    bar.style.height = "0";
+    bar.style.setProperty("--bar-height", value + "px"); // Set individual bar height
     container.appendChild(bar);
+    observer.observe(bar);
   });
 });
