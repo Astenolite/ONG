ec46a83d8494151cd019a9f0d8c86210d5d83d48
Astenolite <vladcindea06@gmail.com>
2024-05-11 14:41:29 +0300

    update Index page

--- general/master-header/master-header.js
+++ general/master-header/master-header.js
@@ -3,13 +3,10 @@ function getButton(name) {
 }
 
 let currentButton;
-let pageList = ['about-us-page','project-page', 'donate-page', 'volunteer-page', 'contact-page' ];
-let buttonList = [getButton('about-us-button'), getButton('project-button'), getButton('donate-button'), getButton('volunteer-button'), getButton('contact-button')];
+let pageList = ['index','project-page', 'donate-page', 'volunteer-page', 'contact-page' ];
+let buttonList = [getButton('index-button'), getButton('project-button'), getButton('donate-button'), getButton('volunteer-button'), getButton('contact-button')];
 
-/*
-for(let i=0; i<buttonList.length; i++){
-  console.log(buttonList[i]);
-}*/
+
 
 const currPage_Header = window.getRelativePath().page;
 console.log(currPage_Header);
@@ -25,10 +22,13 @@ for(let i=0; i<buttonList.length; i++){
 
 const anchorElements = document.querySelector('.master-header').querySelectorAll('a');
 const newRelPath_Header = window.getRelativePath().newPath;
+console.log(newRelPath_Header);
 
 for(const anchor of anchorElements){
   const url = new URL(anchor.href, window.location.href);
   const relativePath = url.pathname.substring(1);
 
   anchor.href = newRelPath_Header + relativePath;
+  console.log(newRelPath_Header + " " + relativePath);
+  console.log(anchor.href);
 }
